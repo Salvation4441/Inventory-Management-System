ab458b0a3e0f43ec40be7c1ef1f7168aaf788238
Salvation4441 <morphinerichard@gmail.com>
2025-10-17 13:53:34 +0000

    Worked on profile,settings,login,add new button

--- administrator/urls.py
+++ administrator/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_dashboard, name='admin-dashboard'),
+    path('purchase/',views.purchases,name='purchases'),
+    path('profile/',views.profile,name='profile'),
+    path('sales-report/',views.sales_report,name='sales-report'),
+    path('settings/',views.settings,name='settings')
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 def admin_dashboard(request):
     return render(request,'screens/administrator/dashboard.html')
+
+# purchase page
+def purchases(request):
+    return render(request,'screens/core/purchase.html')
+
+# profile
+def profile(request):
+    return render(request,'screens/core/profile.html')
+
+
+# sales report
+def sales_report(request):
+    return render(request,'screens/administrator/sales-report.html')
+
+# settings page
+def settings(request):
+    return render(request,'screens/administrator/settings.html')
