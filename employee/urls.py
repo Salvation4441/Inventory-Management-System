4c73efecc46bcf7e85ddaf6c855928b6912ab4b6
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2025-10-22 13:23:29 +0000

    Refactor sales management views and templates; remove unused employee sales template, add admin role check in add sales page

--- employee/urls.py
+++ employee/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.employee_dashboard, name='employee-dashboard'),
-    path('employee-sales/', views.employee_sales, name='employee-sales'),
 ]
